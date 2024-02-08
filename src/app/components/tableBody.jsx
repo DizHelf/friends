2d3bb5971d8cd4885f158d359e9ac08c41c1c1f0
DizHelf <mustangzxc@inbox.ru>
2024-02-08 08:52:50 +0300

    add delete button add bookmark in userTable

--- src/app/components/tableBody.jsx
+++ src/app/components/tableBody.jsx
@@ -3,12 +3,27 @@ import PropTypes from "prop-types";
 import _ from "lodash";
 
 const TableBody = ({ column, data }) => {
+
+    const renderFunction = (item, column) => {
+        if (column.component) {
+            const component = column.component;
+            if (typeof component === "function") {
+                return component(item);
+            }
+            return column.component;
+        }
+
+        return _.get(item, column.path);
+    };
+
     return (
         <tbody>
             {data.map(item => 
                 <tr key={item._id}>
                     {column.map((column) => 
-                        <td key={column.name}>{_.get(item, column.path)}</td>
+                        <td key={column.name}>
+                            {renderFunction(item, column)}
+                        </td>
                     )}
                 </tr>
             )}
--- src/app/components/userTable.jsx
+++ src/app/components/userTable.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import User from "./user";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
+import Bookmark from "./bookmark";
 
-const UserTable = ({ users, sortOptions, onSort, ...rest }) => {
+const UserTable = ({ users, sortOptions, onSort, onDelete, onToggleBookMark, ...rest }) => {
 
     const column = [
         { name: "Имя", path: "name" },
@@ -12,8 +12,17 @@ const UserTable = ({ users, sortOptions, onSort, ...rest }) => {
         { name: "Профессия", path: "profession.name" },
         { name: "Встретился, раз", path: "completedMeetings" },
         { name: "Оценка", path: "rate" },
-        { name: "Избранное", path: "bookmark" }
+        { name: "Избранное", path: "bookmark", component: (item) => renderBookmark(item) },
+        { name: "Удалить", component: (item) => renderButton(item) }
     ];
+
+    const renderButton = (item) => {
+       return <button onClick={() => onDelete(item._id)} className="btn btn-danger">delete</button>;
+    };
+
+   const renderBookmark = (item) => {
+        return <Bookmark status={item.bookmark} onClick={() => onToggleBookMark(item._id)}></Bookmark>;
+   };
 
     return (
         <table className="table">
@@ -27,7 +36,9 @@ const UserTable = ({ users, sortOptions, onSort, ...rest }) => {
 UserTable.propTypes = {
     users: PropTypes.array.isRequired,
     sortOptions: PropTypes.object.isRequired,
-    onSort: PropTypes.func.isRequired
+    onSort: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired
 };
 
 export default UserTable;
